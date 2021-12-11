43f5efb9f06bc7256d5d8285f25e851157e3a066
MateuszTk <mateusz.tkaczewski@gmail.com>
2021-12-11 13:38:03 +0100

    Children rotation

---
+++ src/ExampleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleController : MonoBehaviour
+{
+    static public Networking networking;
+    public GameObject obj;
+
+    GameObject inst;
+
+    void Start()
+    {
+        networking = gameObject.AddComponent<Networking>();
+        networking.uri = "https://localhost:5001/";
+        networking.obj = obj;
+        networking.Authorize(networking.StartSyncLoop);
+    }
+
+    public void AddCube()
+    {
+        inst = ObjSync.NetInstantiate(obj, networking, new Vector3(0, 2, 0), Quaternion.identity, true);
+    }
+
+    public void UploadParts()
+    {
+        inst.GetComponent<ObjSync>().UploadChildren();
+    }
+}
